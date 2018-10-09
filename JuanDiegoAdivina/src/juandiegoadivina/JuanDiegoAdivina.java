62ccd4fdd9f4a65ba6fa3a7ba039d8f288d2f956
lunero <lunero@Lunerin>
2018-10-09 22:03:21 +0200

    Variado rango para que calcule los números entre el 10 y el 50 inclusive

--- JuanDiegoAdivina/src/juandiegoadivina/JuanDiegoAdivina.java
+++ JuanDiegoAdivina/src/juandiegoadivina/JuanDiegoAdivina.java
@@ -16,8 +16,8 @@ public class JuanDiegoAdivina {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+       int n = 0,cont = 0, resultado;
+        int x = (int) (41* Math.random() +10);
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
